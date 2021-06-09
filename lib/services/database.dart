6830cd1c749a081f1188156e415195d8e6ca41ae
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2021-06-09 17:01:34 +0200

    setup basic settings page

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:guitar_app/screens/song.dart';
+// import 'package:guitar_app/screens/song.dart';
 
 class DatabaseService {
 
